36602365fdb72c382a09858e545f1ec9d62aee41
wdq <1215971936@qq.com>
2018-05-23 16:23:13 +0800

    tz

--- src/pages/intelligent/house.vue
+++ src/pages/intelligent/house.vue
@@ -1,30 +1,36 @@
 <template>
-  <section class="page-group">
-    <div class="tz-device-house">
-      <h2 class="h2">可选房屋</h2>
+  <div>
+    <section class="page-group">
+      <div class="tz-device-house">
+        <h2 class="h2">可选房屋</h2>
 
-      <ul class="cell-list auto">
-        <li class="item link">
-          <label class="cell">
-            <div class="name">东樾府13栋12东樾府13栋12东樾府13栋12东樾府13栋12东樾府13栋12东樾府13栋12东樾府13栋12-1</div>
-            <input type="radio" name="pay" checked>
-            <div class="radio align-self mr20">
-              <i class="check"></i>
-            </div>
-          </label>
-        </li>
-        <li class="item link">
-          <label class="cell">
-            <div class="name">西城御府2单元1号楼3-3</div>
-            <input type="radio" name="pay">
-            <div class="radio align-self mr20">
-              <i class="check"></i>
-            </div>
-          </label>
-        </li>
-      </ul>
-    </div>
-  </section>
+        <ul class="cell-list auto">
+          <li class="item link">
+            <label class="cell">
+              <div class="name">东樾府13栋12东樾府13栋12东樾府13栋12东樾府13栋12东樾府13栋12东樾府13栋12东樾府13栋12-1</div>
+              <input type="radio" name="pay" checked>
+              <div class="radio align-self mr20">
+                <i class="check"></i>
+              </div>
+            </label>
+          </li>
+          <li class="item link">
+            <label class="cell">
+              <div class="name">西城御府2单元1号楼3-3</div>
+              <input type="radio" name="pay">
+              <div class="radio align-self mr20">
+                <i class="check"></i>
+              </div>
+            </label>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <!--footer-->
+    <footer>
+      <a href="#" class="bottom-fixed btn-orange-fixed">确定</a>
+    </footer>
+  </div>
 </template>
 
 <script>
