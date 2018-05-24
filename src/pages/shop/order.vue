20369ffd7dd78dda64646f1f6baa7f82c7dc44c2
ajay <405507949@qq.com>
2018-05-24 16:49:28 +0800

    打包费

--- src/pages/shop/order.vue
+++ src/pages/shop/order.vue
@@ -73,6 +73,10 @@
           <span>配送费</span>
           <span>¥ {{orderInfo.send_money}}</span>
         </li>
+        <li class="item cell">
+          <span>打包费</span>
+          <span>¥ {{package_price == 0?Number(package_price):package_price.toFixed(2)}}</span>
+        </li>
         <!--<li class="item cell">-->
         <!--<span>优惠活动</span>-->
         <!--<span class="orange">满100减50</span>-->
@@ -92,9 +96,9 @@
         <div class="order-total">
           <p class="f32">合计:
             <i class="icon tp"></i>
-            <span class="orange">{{tcion}}</span>
+            <span class="orange">{{tcion.toFixed(2)}}</span>
           </p>
-          <p>¥{{money}}</p>
+          <p>¥{{money.toFixed(2)}}</p>
         </div>
         <a href="javascript:void(0);" class="btn btn-orange" @click="seve_order">结算</a>
       </div>
@@ -167,7 +171,10 @@
         comment: '',
         sheetShow:false,
         selfMenus:{},
-        mention:''
+        mention:'',
+        package_price: 0,
+        package_tcion: 0
+
       }
     },
     computed: {
@@ -178,16 +185,23 @@
         //立即购买
         this.cartList = storeJs.get('buy_goods');
         console.log(this.cartList);
-        this.money = (this.cartList[0].now_price * this.cartList[0].cart_sum).toFixed(2);
-        this.tcion = (this.cartList[0].now_tcion * this.cartList[0].cart_sum).toFixed(2);
+        this.package_price = this.cartList[0].goods_package_price;
+        this.package_tcion = this.cartList[0].goods_package_tcion;
+        this.money = (this.cartList[0].now_price * this.cartList[0].cart_sum) + parseFloat(this.package_price);
+        this.tcion = (this.cartList[0].now_tcion * this.cartList[0].cart_sum) + parseFloat(this.package_tcion);
+
       } else {
         //选中购物车商品
-        this.money = cart.getMoney(cart.order_pay).price;
-        this.tcion = cart.getMoney(cart.order_pay).tcion;
+
         if (cart.order_pay.length > 0) {
           cart.order_pay.find(item => {
             this.cartList.push(cart.cart_list[item]);
+            this.package_price += parseFloat(cart.cart_list[item].goods_package_price);
+            this.package_tcion += parseFloat(cart.cart_list[item].goods_package_tcion);
           });
+          //console.log(this.cartList);
+          this.money = parseFloat(cart.getMoney(cart.order_pay).price) + this.package_price;
+          this.tcion = parseFloat(cart.getMoney(cart.order_pay).tcion) + this.package_tcion;
         } else {
           this.$router.push('/shop/cart');
         }
