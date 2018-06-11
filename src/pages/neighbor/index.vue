e85efab1d021b0968e32246c80e4857662ed9b6f
ajay <405507949@qq.com>
2018-06-11 11:34:14 +0800

    样式修改

--- src/pages/neighbor/index.vue
+++ src/pages/neighbor/index.vue
@@ -327,12 +327,12 @@
   .my-gallery figure{
     background: #fff;
     margin-top: .4rem;
-    width: 4.32rem;
-    height: 4.32rem;
+    width: 4.1rem;
+    height: 4.1rem;
     line-height: 4.28rem;
     border: 1px solid #e8e8e8;
     float: left;
-    margin-right: .48rem;
+    margin-right: .54rem;
     text-align: center;
   }
   .my-gallery figure img{
