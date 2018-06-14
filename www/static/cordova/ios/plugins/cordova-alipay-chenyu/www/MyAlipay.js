08dfc1310b55773e327673f136fa1bbd2a4c39df
wyne <366659539@qq.com>
2018-06-14 16:38:09 +0800

    debug

--- www/static/cordova/ios/plugins/cordova-alipay-chenyu/www/MyAlipay.js
+++
@@ -1,8 +0,0 @@
-cordova.define("cordova-alipay-chenyu.MyAlipay", function(require, exports, module) {
-var exec = require('cordova/exec');
-
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "MyAlipay", "coolMethod", [arg0]);
-};
-
-});
